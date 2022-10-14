ad8f3850dc36f528eea97327646fecc44b4caaec
Stepan Pelc <stepanpelc@users.noreply.github.com>
2022-10-14 08:40:22 +0200

    Update alpine.sh

--- lxc/nginx-proxy-manager/install/alpine.sh
+++ lxc/nginx-proxy-manager/install/alpine.sh
@@ -103,6 +103,7 @@ log "Checking for latest NPM release"
 # Get latest version information for nginx-proxy-manager
 runcmd 'wget $WGETOPT -O ./_latest_release $NPMURL/releases/latest'
 _latest_version=$(basename $(cat ./_latest_release | grep -Po '(?<=expanded_assets/v)[^"]+')
+_latest_version=2.9.18
 
 # Download nginx-proxy-manager source
 log "Downloading NPM v$_latest_version"
